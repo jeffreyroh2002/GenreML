4cab2e8d0c89e0b1482cd9e1a3a6d35a32a1e919
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-03-27 07:03:55 +0000

    Skeleton of CNN completed. First compilation: Accuracy 93.29% but Loss: 0.52

--- Stage0/Testing_NN_Techniques.py
+++ Stage0/Testing_NN_Techniques.py
@@ -12,6 +12,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import tensorflow as tf
 from tensorflow import keras
+from keras.models import Sequential
 
 df = pd.read_csv('../input_data/features_3_sec.csv')
 #print(df.head())
@@ -49,5 +50,42 @@ print(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+#Building the Model
+def trainModel(model, epochs, optimizer):
+    batch_size = 128
+    model.compile(optimizer = optimizer,
+                  loss = 'sparse_categorical_crossentropy',
+                  metrics = 'accuracy'
+    )
+    return model.fit(X_train, y_train, 
+                     validation_data=(X_test, y_test),
+                     epochs = epochs, batch_size = batch_size)
 
+def plotValidate(history):
+    print("Validation Accuracy", max(history.history["val_accuarcy"]))
+    pd.DataFrame(history.history).plot(figsize=(12,6))
+    plt.show()
+    
+cnn_model = keras.models.Sequential([
+    keras.layers.Dense(512, activation = 'relu', input_shape= (X_train.shape[1],)),  #??
+    keras.layers.Dropout(0.2),  # heart of CNN
+
+    keras.layers.Dense(256, activation = 'relu'),
+    keras.layers.Dropout(0.2), 
+    
+    keras.layers.Dense(128, activation = 'relu'),
+    keras.layers.Dropout(0.2), 
+    
+    keras.layers.Dense(64, activation = 'relu'),
+    keras.layers.Dropout(0.2), 
+    
+    keras.layers.Dense(10, activation = 'softmax'),
+])
+print(cnn_model.summary())
+model_history = trainModel(model=cnn_model, epochs=600, optimizer='adam')
+
+#model evaluation
+test_loss, test_acc = cnn_model.evaluate(X_test, y_test, batch_size=120)
+print("The test Loss is :", test_loss)
+print("The Best test Accuracy is :", test_acc*100)
 
