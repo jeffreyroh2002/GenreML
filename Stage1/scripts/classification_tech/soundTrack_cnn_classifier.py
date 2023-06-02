f30c06d2975dfecd9aa56bbffab3cfe449eb04af
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-06-02 13:25:29 +0000

    adjusted script for heatmap label coherency

--- Stage1/scripts/classification_tech/soundTrack_cnn_classifier.py
+++ Stage1/scripts/classification_tech/soundTrack_cnn_classifier.py
@@ -16,7 +16,7 @@ SAVE_MODEL = True
 SAVE_HM = True
 
 #OUTPUT DIR/FILE NAMES
-NEWDIR_NAME = "mood_cnn-0601-testing"
+NEWDIR_NAME = "testing_labels"
 
 MODEL_NAME = "saved_model"
 HM_NAME = "heatmap.png"
@@ -37,10 +37,13 @@ def load_data(data_path):
     # convert lists to numpy arrays
     X = np.array(data["mfcc"])
     y = np.array(data["labels"])
+    label_list = data.get("mapping", {})  # ['Anger', 'Fear', ...]
+
+    print(label_list)
 
     print("Data succesfully loaded!")
 
-    return  X, y
+    return  X, y, label_list
 
 def save_plot(history, newdir_path=NEWDIR_PATH, a_plot_name=A_PLOT_NAME, l_plot_name=L_PLOT_NAME):
 
@@ -64,28 +67,26 @@ def save_plot(history, newdir_path=NEWDIR_PATH, a_plot_name=A_PLOT_NAME, l_plot_
     plt.legend(['train','val'], loc='upper left')
     plt.savefig(os.path.join(newdir_path, l_plot_name))
     
-def get_heatmap(model, X_test, y_test, newdir_path, hm_name):
+def get_heatmap(model, X_test, y_test, newdir_path, hm_name, label_list):
     
     plt.figure()
     
-    #extracting predictions of X_test
     prediction = model.predict(X_test)
     y_pred = np.argmax(prediction, axis=1)
-    #cm = confusion_matrix(y_test, y_pred)
-        
-    labels = unique_labels(y_test)
+
+    labels = sorted(label_list)  # Sort the labels
     column = [f'Predicted {label}' for label in labels]
     indices = [f'Actual {label}' for label in labels]
     table = pd.DataFrame(confusion_matrix(y_test, y_pred), columns=column, index=indices)
+
     hm = sns.heatmap(table, annot=True, fmt='d', cmap='viridis')
-        
     plt.savefig(os.path.join(NEWDIR_PATH, hm_name))
     print("heatmap generated and saved in {path}".format(path=NEWDIR_PATH))
     
 def prepare_datasets(test_size, validation_size):
     
     # load data
-    X, y = load_data(DATA_PATH)
+    X, y, label_list = load_data(DATA_PATH)
     
     # create train, validation and test split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
@@ -96,7 +97,7 @@ def prepare_datasets(test_size, validation_size):
     X_validation = X_validation[..., np.newaxis]
     X_test = X_test[..., np.newaxis]
     
-    return X_train, X_validation, X_test, y_train, y_validation, y_test
+    return X_train, X_validation, X_test, y_train, y_validation, y_test, label_list
     
 def build_model(input_shape):
     """Generates CNN model
@@ -156,7 +157,7 @@ def predict(model, X, y):
 if __name__ == "__main__":
 
     #create train, val, test sets
-    X_train, X_validation, X_test, y_train, y_validation, y_test = prepare_datasets(0.25, 0.2)
+    X_train, X_validation, X_test, y_train, y_validation, y_test, label_list = prepare_datasets(0.25, 0.2)
 
     # create network
     input_shape = (X_train.shape[1], X_train.shape[2], 1)
@@ -171,7 +172,7 @@ if __name__ == "__main__":
     model.summary()
 
     # train model
-    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=32, epochs=150, verbose=1)
+    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=32, epochs=5, verbose=1)
     print("Finished Training Model!")
     
     #printing val loss and accuracy
@@ -200,4 +201,4 @@ if __name__ == "__main__":
 
     # output heatmap
     if (SAVE_HM == True):
-        get_heatmap(model, X_test, y_test, NEWDIR_PATH, HM_NAME)
+        get_heatmap(model, X_test, y_test, NEWDIR_PATH, HM_NAME, label_list)
