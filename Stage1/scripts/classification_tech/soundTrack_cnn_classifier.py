55b9dd9a3f6ecc5e014c5a068daf39ec0555e642
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-06-02 13:26:20 +0000

    changed epochs to 150 (need to test)

--- Stage1/scripts/classification_tech/soundTrack_cnn_classifier.py
+++ Stage1/scripts/classification_tech/soundTrack_cnn_classifier.py
@@ -172,7 +172,7 @@ if __name__ == "__main__":
     model.summary()
 
     # train model
-    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=32, epochs=5, verbose=1)
+    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=32, epochs=150, verbose=1)
     print("Finished Training Model!")
     
     #printing val loss and accuracy
