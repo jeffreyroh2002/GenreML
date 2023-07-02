c10335bec06f88febcfb00c1b894bf10f427d27e
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-07-02 05:04:13 +0000

    changed paths

--- Stage1/scripts/preprocessing/GTZAN_data_preprocessing/check_corrupt_file.py
+++ Stage1/scripts/preprocessing/GTZAN_data_preprocessing/check_corrupt_file.py
@@ -7,9 +7,8 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import json
 
-PATH = "/workspace/MusicML/Stage1/audio_file/preprocessed/corrupted_file_idx.npz"
-DIR_NAME = "/workspace/MusicML2/Stage1/audio_file/raw_imported/Data/genres_original"
-
+PATH = "/workspace/MusicML2/Stage1/audio_file/preprocessed/corrupted_file_idx.npz"
+DIR_NAME = "/workspace/MusicML2/Stage1/audio_file/raw_imported/GTZAN_full/genres_original"
 
 def pre_setting():
     # Save audio paths and labels
