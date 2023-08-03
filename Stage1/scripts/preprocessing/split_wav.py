3b460c9ee8efd0e4b439179a5c20e01e4751165b
송재휘 <wotns0319@naver.com>
2023-08-03 13:06:03 +0000

    new data

--- Stage1/scripts/preprocessing/split_wav.py
+++ Stage1/scripts/preprocessing/split_wav.py
@@ -5,6 +5,7 @@ import random
 PATH = "/workspace/MusicML2/Stage1/audio_file/raw_imported/genres_original"
 OUT_PATH = "/workspace/MusicML2/Stage1/audio_file/raw_imported/GTZAN_3sec"
 
+
 # /workspace/MusicML2/Stage1/audio_file/raw_imported/Data/genres_original/blues/blues.00000.wav
 if not os.path.exists(OUT_PATH):
     os.makedirs(OUT_PATH)
@@ -17,15 +18,17 @@ def split_wav_file(audio_path):
     genre_path = os.path.join(OUT_PATH,genre)
     if not os.path.exists(genre_path):
         os.makedirs(genre_path)
+    
     for i in range(3):
-        random_seed.append(random.randint(1, 19))
-        
+        random_seed.append(random.randint(0, 18))
+    
     for i in range(19):
-        if i in random_seed:
+        if i in random_seed: 
             t1 = i * 1.5
             t2 = (i+2) * 1.5
             t1 = t1 * 1000 #Works in milliseconds
             t2 = t2 * 1000
+
 
             file_name = f_path[-1]
             file_name = file_name.split(".")
@@ -35,10 +38,10 @@ def split_wav_file(audio_path):
             newAudio = AudioSegment.from_wav(audio_path)
             newAudio = newAudio[t1:t2]
             newAudio.export(os.path.join(genre_path,file_name), format="wav") #Exports to a wav file in the current path.
-
+    
+    # print(genre + " has been splitted!");
     
 if __name__ == "__main__":
     for (root, dirs, files) in os.walk(PATH):
         for filename in files:
             split_wav_file(root + "/" + filename)
-        print(dirs + " has completed!")    
